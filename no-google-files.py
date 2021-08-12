b685b551ce86158fec098e1289385804bbc4b533
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2021-08-12 17:50:29 +0200

    Automate categories (#68)
    * Initital commit (Work in progress)
    
    * wip
    
    * Finished script
    
    Co-authored-by: Nick Spaargaren <nickspaargaren@users.noreply.github.com>

---
+++ no-google-files.py
@@ -0,0 +1,34 @@
+from datetime import date
+today = date.today()
+
+class temporary:
+  title = ''
+  categories = []
+
+def Create(title, categories):
+  file_name = 'categories/'+title.strip('#').rstrip('\n').replace(' ', '').lower()
+
+  newfile = open(file_name + '.txt', 'w')
+  newfile.write('#This blocklist helps Pi-hole\'s admin restrict access to Google and its domains.'+'\n')
+  newfile.write('#Last updated: ' + today.strftime('%d-%m-%Y') +'\n')
+  newfile.write(title +'\n')
+
+  for url in categories:
+    newfile.write('0.0.0.0 ' + url + '\n')
+
+
+with open('pihole-google.txt', 'r') as main:
+
+  for line in main:
+
+    if '#' in line:
+
+      if temporary.title and temporary.categories:
+        Create(temporary.title, temporary.categories)
+        temporary.title = ''
+        temporary.categories = []
+
+      temporary.title = line
+
+    elif not '#' in line:
+      temporary.categories.append(line.rstrip('\n'))
