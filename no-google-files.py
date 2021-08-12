023cc9a4ce9dec6eb9375f4eea68f90a374fcdae
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2021-08-12 17:30:05 +0200

    wip

--- no-google-files.py
+++ no-google-files.py
@@ -1,14 +1,42 @@
-with open('pihole-google.txt', 'r') as f:
+from datetime import date
+today = date.today()
 
-  
-  for line in f:
-    if '#' in line:
-      file_name = "categories/"+line.strip("#").rstrip("\n").replace(" ", "").lower()
-      print(file_name)
-      
+class temporary:
+  title = ''
+  categories = []
 
-      newfile = open(file_name + ".txt", "w")
+def Create(title, categories):
+  file_name = 'categories/'+title.strip('#').rstrip('\n').replace(' ', '').lower()
 
-      newfile.write('test')
-      
-  
+  newfile = open(file_name + '.txt', 'w')
+  newfile.write('#This blocklist helps Pi-hole\'s admin restrict access to Google and its domains.'+'\n')
+  newfile.write('#Last updated: ' + today.strftime('%d-%m-%Y') +'\n')
+  newfile.write(title +'\n')
+
+  for url in categories:
+    newfile.write('0.0.0.0 ' + url + '\n')
+
+
+
+with open('pihole-google.txt', 'r') as main:
+
+  for line in main:
+    
+    if '#' in line: # start
+      # print(temporary.title)
+      # print(temporary.categories)
+
+      if temporary.title and temporary.categories:
+        Create(temporary.title, temporary.categories)
+        # break
+
+      temporary.title = line
+
+      if temporary.categories:
+        Create(temporary.title, temporary.categories)
+        temporary.title = ''
+        temporary.categories = []
+
+
+    elif not '#' in line:
+      temporary.categories.append(line.rstrip('\n'))
