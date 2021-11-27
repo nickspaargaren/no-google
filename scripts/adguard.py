98b2337c15fab37cf06301eaa4fe9be8604975de
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2021-11-27 17:58:27 +0100

    Update workflow paths

--- scripts/adguard.py
+++ scripts/adguard.py
@@ -3,11 +3,11 @@ today = date.today()
 
 file_name = 'pihole-google-adguard'
 
-newfile = open('./' + file_name + '.txt', 'w')
+newfile = open('../' + file_name + '.txt', 'w')
 newfile.write('! This blocklist helps Pi-hole\'s admin restrict access to Google and its domains.'+'\n')
 newfile.write('! Last updated: ' + today.strftime('%d-%m-%Y') +'\n')
 
-with open('./pihole-google.txt', 'r') as main:
+with open('../pihole-google.txt', 'r') as main:
 
   for line in main:
     if '#' in line:
