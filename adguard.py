cb252a303b58343ecd68931c44934e308d22c0c9
Nick Spaargaren <nickspaargaren@protonmail.com>
2021-11-27 17:33:25 +0100

    Create and automate AdGuard list (#73)
    * Add automated AdGuard list
    * Automated AdGuard list

---
+++ adguard.py
@@ -0,0 +1,16 @@
+from datetime import date
+today = date.today()
+
+file_name = 'pihole-google-adguard'
+
+newfile = open(file_name + '.txt', 'w')
+newfile.write('! This blocklist helps Pi-hole\'s admin restrict access to Google and its domains.'+'\n')
+newfile.write('! Last updated: ' + today.strftime('%d-%m-%Y') +'\n')
+
+with open('pihole-google.txt', 'r') as main:
+
+  for line in main:
+    if '#' in line:
+      newfile.write('! ' + line[2:])
+    elif not '#' in line:
+      newfile.write('||' + line.rstrip("\n") + '^' + '\n')
