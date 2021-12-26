fc86cdbf5b8dd73e6181344433d8615250a0b5bb
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2021-12-26 18:32:42 +0100

    Remove unnecessary line checks

--- scripts/domain-check-api.py
+++ scripts/domain-check-api.py
@@ -8,24 +8,22 @@ load_dotenv()
 with open('../check-domains.txt', 'r') as main:
 
   for line in main:
-    if not '#' in line and not ':' in line:
-      if line != '\n':
-        response = requests.get("https://www.whoisxmlapi.com/whoisserver/WhoisService",
-          {
-            'domainName' : line,
-            'outputFormat': 'json',
-            'apiKey' : os.getenv('whoisxmlapikey'),
-            'thinWhois' : 'true'
-          }
-        )
+    response = requests.get("https://www.whoisxmlapi.com/whoisserver/WhoisService",
+      {
+        'domainName' : line,
+        'outputFormat': 'json',
+        'apiKey' : os.getenv('whoisxmlapikey'),
+        'thinWhois' : 'true'
+      }
+    )
 
-        data=response.json()
+    data=response.json()
 
-        print(
-          json.dumps(
-            {
-              'domain': line.rstrip("\n"),
-              'hostNames': data['WhoisRecord']['registryData']['nameServers']['hostNames']
-            }, indent=4
-          )
-        )
+    print(
+      json.dumps(
+        {
+          'domain': line.rstrip("\n"),
+          'hostNames': data['WhoisRecord']['registryData']['nameServers']['hostNames']
+        }, indent=4
+      )
+    )
