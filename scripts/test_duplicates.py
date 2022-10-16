4cf5435adb98c9a856622517d26e354081fdd9ee
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2022-10-16 10:42:48 +0200

    Add check for regex domains

--- scripts/test_duplicates.py
+++ scripts/test_duplicates.py
@@ -12,6 +12,10 @@ f.close()
 for domain, count in counts.items():
     if count > 1:
         assert False, domain + " occurred more than one time in pihole-google.txt, please remove duplicate domains."
+    if ".l.google.com" in domain:
+        assert False, "A l.google.com domain occurred in pihole-google.txt, please remove regex domains."
+    if ".googlevideo.com" in domain:
+        assert False, "A googlevideo.com domain occurred in pihole-google.txt, please remove regex domains."
 
 def test_succes():
     assert(True)
