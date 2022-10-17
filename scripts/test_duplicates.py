8014940ad67598b6e8824b7d73e4d3d3e8e7b4bb
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2022-10-17 09:00:13 +0200

    Split duplicates & regex tests

--- scripts/test_duplicates.py
+++ scripts/test_duplicates.py
@@ -1,3 +1,14 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--type',
+                    nargs='?',
+                    choices=['duplicates', 'regex'],
+                    help='Test Type')
+
+args = parser.parse_args()
+
 counts = { }
 with open("../pihole-google.txt") as f: 
     for line in f:
@@ -9,13 +20,17 @@ with open("../pihole-google.txt") as f:
                 counts[domain] = 1 # new entry 
 f.close()
 
-for domain, count in counts.items():
-    if count > 1:
-        assert False, domain + " occurred more than one time in pihole-google.txt, please remove duplicate domains."
-    if ".l.google.com" in domain:
-        assert False, "A l.google.com domain occurred in pihole-google.txt, please remove regex domains."
-    if ".googlevideo.com" in domain:
-        assert False, "A googlevideo.com domain occurred in pihole-google.txt, please remove regex domains."
+if args.type == "duplicates":
+    for domain, count in counts.items():
+        if count > 1:
+            assert False, domain + " occurred more than one time in pihole-google.txt, please remove duplicate domains."
+
+if args.type == "regex":
+    for domain, count in counts.items():
+        if ".l.google.com" in domain:
+            assert False, "A l.google.com domain occurred in pihole-google.txt, please remove regex domains."
+        if ".googlevideo.com" in domain:
+            assert False, "A googlevideo.com domain occurred in pihole-google.txt, please remove regex domains."
 
 def test_succes():
     assert(True)
