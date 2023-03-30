476145e29c00b0b3d0f27ce2e72690bcc60dabeb
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2023-03-30 23:03:07 +0200

    Add duplicate removals & fetch whois data

--- scripts/domain-check-api.py
+++ scripts/domain-check-api.py
@@ -1,16 +1,32 @@
-import whois # pip install python-whois
+import whois  # pip install python-whois
 import time
-
 import re
 
+
+def get_domain(url: str) -> str:
+    """
+    A function that extracts the domain name from a `url`
+    """
+    domain = re.findall("(?<=\.)[^.]+\.[^.]+$", url)
+    if domain != []:
+        return domain[0]
+    else:
+        return url
+
+
+def remove_duplicates(mylist: list[str]):
+    return list(dict.fromkeys(mylist))
+
+
 def get_domains():
-  with open('../pihole-google.txt', 'r') as main:
-    for line in main:
-      if not '#' in line and not ':' in line:
-        line = re.findall("(?<=\.)[^.]+\.[^.]+$", line)
-        if line != '\n' and line != []:
-            line = re.sub(r"\n", "", line[0])
-            yield line
+    with open('../pihole-google.txt', 'r') as main:
+        for line in main:
+            if not '#' in line and not ':' in line:
+                line = get_domain(line)
+                if line != '\n' and line != []:
+                    line = re.sub(r"\n", "", line)
+                    yield line
+
 
 def is_registered(domain):
     """
@@ -24,6 +40,9 @@ def is_registered(domain):
     else:
         return [w.domain_name, w.org, w.name_servers]
 
+
 # iterate over domains
-for domain in get_domains():
+domains = remove_duplicates(get_domains())
+
+for domain in domains:
     print(domain, is_registered(domain.rstrip("\n")))
