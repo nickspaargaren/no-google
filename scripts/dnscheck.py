39ba6b79ce0304f405c879324451818d154a0f51
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2024-07-29 09:43:37 +0200

    Remove NoAnswer and Timeout from dns response exceptions

--- scripts/dnscheck.py
+++ scripts/dnscheck.py
@@ -1,14 +1,16 @@
 import dns.resolver
 
+
 def check_domain(domain):
     try:
         # Query for 'A' records
-        answers = dns.resolver.resolve(domain, 'A')
+        answers = dns.resolver.resolve(domain, "A")
         # If we get any answers, the domain has a response
         return bool(answers)
-    except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN, dns.resolver.Timeout):
+    except dns.resolver.NXDOMAIN:
         # No answer or domain does not exist or request timed out
         return False
+
 
 def main():
     with open("../pihole-google.txt") as f:
@@ -17,6 +19,7 @@ def main():
             if domain and not domain.startswith("#"):
                 if not check_domain(domain):
                     print(f"No DNS response for domain: {domain}")
+
 
 if __name__ == "__main__":
     main()
