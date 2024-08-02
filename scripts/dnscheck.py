1eb6688a4b2efe86a3cdb97bb8f57b543a4330f7
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2024-08-02 22:40:41 +0200

    Update dnscheck command with ns records and assertion

--- scripts/dnscheck.py
+++ scripts/dnscheck.py
@@ -1,25 +1,40 @@
 import dns.resolver
-
+import sys
 
 def check_domain(domain):
     try:
         # Query for 'A' records
-        answers = dns.resolver.resolve(domain, "A")
-        # If we get any answers, the domain has a response
-        return bool(answers)
-    except dns.resolver.NXDOMAIN:
-        # No answer or domain does not exist or request timed out
-        return False
+        dns.resolver.resolve(domain, "A")
+        has_a_record = True
+    except (dns.resolver.NXDOMAIN, Exception):
+        has_a_record = False
+    
+    try:
+        # Query for 'NS' records
+        dns.resolver.resolve(domain, "NS")
+        has_ns_record = True
+    except (dns.resolver.NXDOMAIN, Exception):
+        has_ns_record = False
 
+    return not has_a_record and not has_ns_record
 
 def main():
+    found_domains = 0
+    
     with open("../pihole-google.txt") as f:
         for line in f:
             domain = line.strip()  # strip whitespace
             if domain and not domain.startswith("#"):
-                if not check_domain(domain):
-                    print(f"No DNS response for domain: {domain}")
-
+                if check_domain(domain):
+                    print(f"Domain with no A or NS records: {domain}")
+                    found_domains += 1
+                    if found_domains >= 10:  # Exit early to reduce requests
+                        sys.exit(1)
+    
+    if found_domains > 0:
+        sys.exit(1)
+    else:
+        sys.exit(0)
 
 if __name__ == "__main__":
     main()
