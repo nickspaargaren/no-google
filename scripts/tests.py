8a30411a5d091f918545eb129f585a5d5ef896df
Nick Spaargaren <20847106+nickspaargaren@users.noreply.github.com>
2024-08-14 10:02:52 +0200

    Add code style check and fix commands (#210)
    * Add code style check and fix commands
    * Fix code style

--- scripts/tests.py
+++ scripts/tests.py
@@ -2,35 +2,42 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--type',
-                    nargs='?',
-                    choices=['duplicates', 'regex'],
-                    help='Test Type')
+parser.add_argument(
+    "--type", nargs="?", choices=["duplicates", "regex"], help="Test Type"
+)
 
 args = parser.parse_args()
 
-counts = { }
-with open("../pihole-google.txt") as f: 
+counts = {}
+with open("../pihole-google.txt") as f:
     for line in f:
-        domain = line.strip() # strip whitespace
+        domain = line.strip()  # strip whitespace
         if domain:
-            if domain in counts: # duplicate line, inc count
-                counts[domain] = counts[domain]+1 
+            if domain in counts:  # duplicate line, inc count
+                counts[domain] = counts[domain] + 1
             else:
-                counts[domain] = 1 # new entry 
+                counts[domain] = 1  # new entry
 f.close()
 
 if args.type == "duplicates":
     for domain, count in counts.items():
         if count > 1:
-            assert False, domain + " occurred more than one time in pihole-google.txt, please remove duplicate domains."
+            assert False, (
+                domain
+                + " occurred more than one time in pihole-google.txt, please remove duplicate domains."
+            )
 
 if args.type == "regex":
     for domain, count in counts.items():
         if ".l.google.com" in domain:
-            assert False, "A l.google.com domain occurred in pihole-google.txt, please remove regex domains."
+            assert (
+                False
+            ), "A l.google.com domain occurred in pihole-google.txt, please remove regex domains."
         if ".googlevideo.com" in domain:
-            assert False, "A googlevideo.com domain occurred in pihole-google.txt, please remove regex domains."
+            assert (
+                False
+            ), "A googlevideo.com domain occurred in pihole-google.txt, please remove regex domains."
+
 
 def test_succes():
-    assert(True)
+    assert True
